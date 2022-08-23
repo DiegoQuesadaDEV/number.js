680e6152e64a47eedc3671bd89b966c82484f3a0
DiegoQuesadaDEV <110751513+DiegoQuesadaDEV@users.noreply.github.com>
2022-08-23 13:20:46 +0200

    Add files via upload

---
+++ number.js
@@ -0,0 +1,6 @@
+let altura_cm = 179;
+let altura_m = 1.79;
+let peso_kg = 60.4;
+let altura_redondeada = altura_m.toFixed(1);
+let peso_redondeado = peso_kg.toFixed(0);
+let maximo_valor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
